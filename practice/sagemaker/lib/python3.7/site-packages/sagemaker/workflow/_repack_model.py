999379220f8b90dd3e5cbbe098b91ca2784c7120
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-20 20:07:39 +0000

    Adding tarfile member sanitization to extractall()

--- practice/sagemaker/lib/python3.7/site-packages/sagemaker/workflow/_repack_model.py
+++ practice/sagemaker/lib/python3.7/site-packages/sagemaker/workflow/_repack_model.py
@@ -55,7 +55,26 @@ if __name__ == "__main__":
         # extract the contents of the previous training job's model archive to the "src"
         # directory of this training job
         with tarfile.open(name=local_path, mode="r:gz") as tf:
-            tf.extractall(path=src_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tf, path=src_dir)
 
         # generate a path to the custom inference script
         entry_point = os.path.join("/opt/ml/code", args.inference_script)
